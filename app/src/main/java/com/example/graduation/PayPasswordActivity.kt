6a202f2cd3b735fb2ac7b98d519fd3ce3c3faf54
yookyung-lee <yookyung.dev@gmail.com>
2024-01-17 21:02:43 +0900

    ini commit

---
+++ app/src/main/java/com/example/graduation/PayPasswordActivity.kt
@@ -0,0 +1,11 @@
+package com.example.graduation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PayPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pay_password)
+    }
+}
