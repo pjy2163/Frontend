eac19aa650a808ed28da13a63bd574d2cf51788e
hong00z <hong00z@naver.com>
2024-02-28 16:22:31 +0900

    로그인 음성메세지 수정 회원가입(이메일, 비밀번호, 비밀번호 확인)

--- app/src/main/java/com/example/graduation/Login_Pwd.kt
+++ app/src/main/java/com/example/graduation/Login_Pwd.kt
@@ -1,5 +1,6 @@
 package com.example.graduation
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,45 +10,49 @@ import com.example.graduation.databinding.ActivityLoginPwdBinding
 import java.security.MessageDigest
 import java.util.*
 
-class Login_Pwd : AppCompatActivity(), TextToSpeech.OnInitListener {
+class Login_Pwd : AppCompatActivity() {
     private lateinit var binding: ActivityLoginPwdBinding
-    private lateinit var textToSpeech: TextToSpeech
-    /* val db = "DB 경로" */
+    lateinit var mtts:TextToSpeech
+    // SharedPreferences에서 소리 on/off 상태 불러오기
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginPwdBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_login_pwd)
         setContentView(binding.root)
-        /*auth = com.google.firebase.ktx.Firebase.auth */
+        // SharedPreferences에서 소리 on/off 상태 불러오기
+        val sharedPreferences = getSharedPreferences("sp1", Context.MODE_PRIVATE)
+        val soundState = sharedPreferences.getBoolean("soundState", false)
 
-        textToSpeech = TextToSpeech(this, this)
+        mtts = TextToSpeech(this) { //모든 글자를 소리로 읽어주는 tts
+            mtts.language = Locale.KOREAN //언어:한국어
+        }
+
+        //화면 정보 읽기
+        if (soundState) {
+            onSpeech("로그인 비밀번호 입력 화면입니다.")
+        }
+
         binding.enter.setOnClickListener {
             val pwd = binding.loginInputPwd.text.toString()
             val email = intent.getStringExtra("email")
             if (pwd == "") {
                 //비밀번호 공백 입력 시
                 Toast.makeText(this, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
-                speakOut("비밀번호를 입력해주세요.")
+                if (soundState) {
+                    onSpeech("비밀번호를 입력해주세요.")
+                }
             }
             else {
-                //입력한 비밀번호를 해시하여 DB에 저장된 비밀번호와 비교
-                val hashedPwd = hashPassword(pwd)
-                val dbPassword = "password" /* <= "email에 해당하는 사용자의 DB 경로".get("password").toString() */
-                if (hashedPwd == dbPassword) {
-                    /* 로그인 로직
-                    auth.signInWithEmailAndPassword(email, pwd)
-                        .addOnCompleteListener(this) { task ->
-                            if (task.isSuccessful) {
-                                LoginInformation.setCurrentLoginUserWithUID(auth.currentUser!!.uid, 1)
-                                val intent = Intent(this, MainActivity::class.java)
-                                startActivity(intent)
-                            }
-                        } */
+                //입력한 비밀번호를 DB에 저장된 비밀번호와 비교
+                val dbPassword = TODO("입력한 email에 해당하는 사용자의 DB 경로.get(password).toString()")
+                if (pwd == dbPassword) {
+                    TODO("로그인 로직")
                 }
                 else {
                     Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
-                    speakOut("비밀번호가 일치하지 않습니다.") }
+                    if (soundState) {
+                        onSpeech("비밀번호가 일치하지 않습니다.")
+                    } }
             }
         }
 
@@ -55,43 +60,14 @@ class Login_Pwd : AppCompatActivity(), TextToSpeech.OnInitListener {
         /* binding.resetPwd.setOnClickListener {
             val intent = Intent("비밀번호 찾기 화면"::class.java)
             startActivity(intent)
+            if (soundState) {
+                    onSpeech("비밀번호 찾기")
+                }
         }*/
     }
 
     //음성 안내
-    override fun onDestroy() {
-        if (textToSpeech != null) {
-            textToSpeech.stop()
-            textToSpeech.shutdown()
-        }
-        super.onDestroy()
-    }
-
-    override fun onInit(status: Int) {
-        if (status == TextToSpeech.SUCCESS) {
-            val result = textToSpeech.setLanguage(Locale.KOREAN)
-            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                Toast.makeText(this, "언어 데이터가 없거나 지원되지 않습니다.", Toast.LENGTH_SHORT).show()
-            }
-        } else {
-            Toast.makeText(this, "TextToSpeech 초기화 실패", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun speakOut(text: String) {
-        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
-    }
-
-    //입력한 비밀번호를 해시하는 함수
-    private fun hashPassword(password: String): String {
-        val md = MessageDigest.getInstance("SHA-256")
-        val hashBytes = md.digest(password.toByteArray())
-        val hexString = StringBuilder()
-        for (byte in hashBytes) {
-            val hex = Integer.toHexString(0xff and byte.toInt())
-            if (hex.length == 1) hexString.append('0')
-            hexString.append(hex)
-        }
-        return hexString.toString()
+    private fun onSpeech(text: CharSequence) {
+        mtts.speak(text.toString(), TextToSpeech.QUEUE_FLUSH, null, null)
     }
 }
--- app/src/main/java/com/example/graduation/Signup_Pwd.kt
+++ app/src/main/java/com/example/graduation/Signup_Pwd.kt
@@ -1,32 +1,83 @@
 package com.example.graduation
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
 import com.example.graduation.databinding.ActivitySignupPwdBinding
+import java.util.Locale
 
 class Signup_Pwd : AppCompatActivity(), SignupDialogInterface {
 
     private lateinit var binding: ActivitySignupPwdBinding
+    lateinit var mtts:TextToSpeech
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupPwdBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        // SharedPreferences에서 소리 on/off 상태 불러오기
+        val sharedPreferences = getSharedPreferences("sp1", Context.MODE_PRIVATE)
+        val soundState = sharedPreferences.getBoolean("soundState", false)
 
-        clickViewEvents()}
+        mtts = TextToSpeech(this) { //모든 글자를 소리로 읽어주는 tts
+            mtts.language = Locale.KOREAN //언어:한국어
+        }
 
-    private fun clickViewEvents() {
+        //화면 정보 읽기
+        if (soundState) {
+            onSpeech("회원가입 비밀번호 입력 화면입니다. 숫자와 영문이 포함된 8~16자리를 입력해주세요.")
+        }
+
         binding.enterButton.setOnClickListener {
-            //비밀번호 조건 미충족 시 (추후 작성)
-            run {
-                val dialog = SignupDialog(this, "숫자, 영문이 포함된\n8~16자리를 입력해주세요.")
-                dialog.isCancelable = false
-                dialog.show(this.supportFragmentManager, "SignupDialog")
+            val pwd = binding.signupInputPwd.text.toString().trim()
+            if (isPwdQualified(pwd)){
+                //비밀번호 조건 충족 시 저장 후 다음 화면으로 이동
+                val intent = Intent(this, Signup_Checkpwd::class.java)
+                intent.putExtra("pwd", pwd) //비밀번호 값 전달
+                startActivity(intent)
             }
-            //비밀번호 조건 충족 시 저장 후 다음 화면으로 이동(추후 작성)
+            else {
+                //비밀번호 조건 미충족 시
+                run {
+                    val dialog = SignupDialog(this, "숫자, 영문이 포함된\n8~16자리를 입력해주세요.")
+                    dialog.isCancelable = false
+                    dialog.show(this.supportFragmentManager, "SignupDialog")
+                    if (soundState) {
+                        onSpeech("숫자와 영문이 포함된 8~16자리를 입력해주세요.")
+                    }
+                }
+            }
         }
     }
 
     override fun onDialogButtonClick() {
         finish()
+    }
+
+    //비밀번호 조건 검사 (숫자, 영문 조합 8~16자)
+    private fun isPwdQualified(pwd: String): Boolean {
+        var hasLetter = false
+        var hasDigit = false
+        val password = binding.signupInputPwd.text.toString()
+        if (password.length < 8 || password.length > 16) //8~16자 조건
+            return false
+        password.forEach{//숫자, 영문 조합 조건
+            if (it.isDigit())
+            {
+                hasDigit = true
+            }
+            if (it.isLetter())
+            {
+                hasLetter = true
+            }
+        }
+        return hasLetter && hasDigit
+    }
+
+    //음성 안내
+    private fun onSpeech(text: CharSequence) {
+        mtts.speak(text.toString(), TextToSpeech.QUEUE_FLUSH, null, null)
     }
 }
