175fe0ad03149e896444e41cc3d23ab63ba02ff4
yookyung-lee <yookyung.dev@gmail.com>
2024-03-02 20:20:51 +0900

    결제 수단 고르기UI 이미지 빼고 깔끔하게 수정

--- app/src/main/java/com/example/graduation/PaymentMethod.kt
+++ app/src/main/java/com/example/graduation/PaymentMethod.kt
@@ -1,4 +1,9 @@
 package com.example.graduation
 
-//은행 계좌 이미지, 은행이름(하나은행 등), 계좌번호
-data class PaymentMethod(val imageResId: Int, val bank: String, val accountNumber: String)
+//은행 로고 이미지, 은행이름(하나은행 등), 계좌번호
+data class PaymentMethod(
+    val imageResId: Int,
+    val bank: String,
+    val accountNumber: String,
+    var isSelected: Boolean = false
+)
