c8519129046392753852946fc2e48a88fef4d35a
parang <69078515+pjy2163@users.noreply.github.com>
2024-03-09 18:08:18 +0900

    로그인 통신 코드 수정

--- app/src/main/java/com/example/graduation/model/User.java
+++ app/src/main/java/com/example/graduation/model/User.java
@@ -1,8 +1,13 @@
 package com.example.graduation.model;
 
 public class User {
+    private String name;
     private String id; // 이게 id
     private String password;
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public void setId(String id) {
         this.id = id; //이메일 형식의 아이디이므로 id라 지칭함.
@@ -15,7 +20,7 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{" +
+        return "User{" + "name='" + name + '\'' +
                 "id='" + id + '\'' +
                 ", password='" + password +
                 '}';
